16e54647d5649c2d1c543d961cae1e4594e0319c
Fatma Shehata <fatmashehata@gmail.com>
2026-01-08 16:05:47 +0200

    Update files

--- src/main/java/utils/reader/JsonUtils.java
+++ src/main/java/utils/reader/JsonUtils.java
@@ -7,17 +7,22 @@ import org.slf4j.LoggerFactory;
 import java.io.InputStream;
 
 public class JsonUtils {
+
     private static final Logger log = LoggerFactory.getLogger(JsonUtils.class);
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    public static <T> T getTestData(String path, Class<T> type){
-        try(InputStream stream = ResourceLoader.getResource(path)){
+    public static <T> T getTestData(String path, Class<T> type) {
+        try (InputStream stream = ResourceLoader.getResource(path)) {
+
+            if (stream == null) {
+                throw new RuntimeException("Test data file not found on classpath: " + path);
+            }
+
             return mapper.readValue(stream, type);
-        }catch (Exception e){
-            log.error("unable to read test data {} ", path, e);
+
+        } catch (Exception e) {
+            log.error("Unable to read test data: {}", path, e);
+            throw new RuntimeException(e); 
         }
-        return null;
     }
-
 }
-
